45beecf6a49f25728dba4220d66c3a6a8621b85e
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-17 23:32:51 +0200

    ddd

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const {StatusCodes} = require('http-status-codes')
-const customError = require('../errors')
+const {StatusCodes} = require('http-status-codes');
+const customError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req,res)=>{
+const uploadProductImageLocal = async (req,res)=>{
     // check if file exists
     // check format
     // check size
@@ -28,4 +30,12 @@ const uploadProductImage = async (req,res)=>{
     return res.status(StatusCodes.OK).json({image:{src:`uploads/${productImage.name}`}})
 }
 
+
+const uploadProductImage = async(req,res)=>{
+
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {use_filename:true, folder:"file-upload",})
+    // console.log(result)
+    fs.unlinkSync(req.files.image.tempFilePath)
+    return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
+}
 module.exports = {uploadProductImage}
